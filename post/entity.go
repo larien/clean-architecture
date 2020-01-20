f5e9ef7fc3051e8c86530c609a30ad854c471c20
Lauren Ferreira <lauren.ferremch@gmail.com>
2020-01-19 23:07:03 -0300

    Fix identation

--- post/entity.go
+++ post/entity.go
@@ -1,11 +1,13 @@
 package post
 
+// import "time"
+
 // Post represents the post entity's attributes.
 type Post struct {
 	ID        uint64    `json:"id"`
 	Title     string    `json:"title"`
 	Content   string    `json:"content"`
-	Author    string      `json:"author"`
+	Author    string    `json:"author"`
 	AuthorID  uint32    `json:"author_id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
