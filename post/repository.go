6a3f3abd99ed824033201a73d5493705d4e1815e
Lauren Ferreira <lauren.ferremch@gmail.com>
2020-01-20 00:03:02 -0300

    Implement Repository layer

---
+++ post/repository.go
@@ -0,0 +1,21 @@
+package post
+
+type Repository interface {
+	// TODO - implement methods
+}
+
+type repository struct {
+	// attributes
+}
+
+func NewRepository() Repository {
+	return repository{
+		// initialized attributes
+	}
+}
+
+func (r *repository) Method(){
+	// method stuff
+}
+
+// methods implemented
