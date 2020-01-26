0f62f905d717519c08cebfee2c5ee35811f6f5de
Lauren Ferreira <lauren.ferremch@gmail.com>
2020-01-26 02:04:34 -0300

    Mock create function for routes layer

---
+++ post/controller_mock.go
@@ -0,0 +1,16 @@
+package post
+
+import (
+	"github.com/stretchr/testify/mock"
+)
+
+// MockController injects mock dependency into controller.
+type MockController struct {
+	mock.Mock
+}
+
+// create represents the mocked method for Create feature in Controller layer
+func (m *MockController) Create(post *Post) error {
+	args := m.Called(post)
+	return args.Error(0)
+}
