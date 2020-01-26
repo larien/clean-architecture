fcbb327fe973138bdb53f670ac81ce7a2f1b2dc5
Lauren Ferreira <lauren.ferremch@gmail.com>
2020-01-26 02:05:10 -0300

    Test create method for routes layer

--- post/routes.go
+++ post/routes.go
@@ -1,9 +1,11 @@
 package post
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/larien/clean-architecture/helper/request"
 	"github.com/larien/clean-architecture/helper/router"
-	"net/http"
 )
 
 // NewRoutes creates a router for Post and sets the endpoints
@@ -17,17 +19,18 @@ func NewRoutes(c Controller) router.Router {
 
 func create(c Controller) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var post Post
+		var post *Post
 		if err := request.Read(r, post); err != nil {
+			log.Printf("an error occurred when parsing JSON: %v", err)
 			request.Error(w, http.StatusBadRequest, err)
 			return
 		}
 
-		// if err := c.Create(); err != nil {
-		// 	log.Printf("an error ocurred when creating a post: %v", err)
-		// 	helper.Error(w, http.StatusBadRequest, err)
-		// 	return
-		// }
+		if err := c.Create(post); err != nil {
+			log.Printf("an error occurred when creating a post: %v", err)
+			request.Error(w, http.StatusInternalServerError, err)
+			return
+		}
 
 		request.Success(w, http.StatusOK, "Post created successfully")
 	}
--- post/routes_unit_test.go
+++ post/routes_unit_test.go
@@ -1,1 +1,106 @@
 package post
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/bxcodec/faker"
+	"github.com/stretchr/testify/mock"
+	assert "github.com/stretchr/testify/require"
+)
+
+func TestPost_NewRouter(t *testing.T) {
+	t.Parallel()
+	t.Run("when Router is created", func(t *testing.T) {
+		t.Parallel()
+		is := assert.New(t)
+
+		c := new(MockController)
+
+		router := NewRoutes(c)
+		is.NotNil(router)
+		is.NotEqual(0, len(router.Routes()))
+	})
+}
+
+func TestPost_create(t *testing.T) {
+	t.Parallel()
+	t.Run("when JSON is invalid", func(t *testing.T) {
+		t.Parallel()
+		is := assert.New(t)
+
+		c := new(MockController)
+
+		invalidJSON := `{ invalid_json:"" }`
+		req := httptest.NewRequest(
+			http.MethodPost,
+			"/posts",
+			bytes.NewBufferString(invalidJSON),
+		)
+		rec := httptest.NewRecorder()
+
+		handler := http.HandlerFunc(create(c))
+		handler.ServeHTTP(rec, req)
+
+		is.Equal(http.StatusBadRequest, rec.Code)
+	})
+	t.Run("when JSON is valid", func(t *testing.T) {
+		t.Parallel()
+		t.Run("and controller failed", func(t *testing.T) {
+			t.Parallel()
+			is := assert.New(t)
+
+			c := new(MockController)
+			defer c.AssertExpectations(t)
+
+			c.On("Create", mock.Anything, mock.Anything).
+				Return(errors.New("error")).
+				Once()
+
+			post := &Post{}
+			err := faker.FakeData(post)
+			is.Nil(err)
+
+			body, _ := json.Marshal(post)
+			req := httptest.NewRequest(http.MethodPost, "/posts", bytes.NewBuffer(body))
+			rec := httptest.NewRecorder()
+
+			handler := http.HandlerFunc(create(c))
+			handler.ServeHTTP(rec, req)
+
+			is.Equal(http.StatusInternalServerError, rec.Code)
+		})
+		t.Run("and controller succeeded", func(t *testing.T) {
+			t.Parallel()
+			is := assert.New(t)
+
+			c := new(MockController)
+			defer c.AssertExpectations(t)
+
+			c.On("Create", mock.Anything, mock.Anything).
+				Return(nil).
+				Once()
+
+			post := &Post{}
+			err := faker.FakeData(post)
+			is.Nil(err)
+
+			body, _ := json.Marshal(post)
+			req := httptest.NewRequest(http.MethodPost, "/posts", bytes.NewBuffer(body))
+			rec := httptest.NewRecorder()
+
+			handler := http.HandlerFunc(create(c))
+			handler.ServeHTTP(rec, req)
+
+			is.Equal(http.StatusOK, rec.Code)
+		})
+	})
+}
+
+// invalid JSON
+
+// valid json
