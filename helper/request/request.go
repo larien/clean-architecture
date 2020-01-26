02d481b3077457d9073a4d8abe368163c8460f76
Lauren Ferreira <lauren.ferremch@gmail.com>
2020-01-26 02:03:34 -0300

    Document helpers/request functions

--- helper/request/request.go
+++ helper/request/request.go
@@ -6,6 +6,12 @@ import (
 	"net/http"
 )
 
+// Read decodes the received body in a request
+func Read(r *http.Request, body interface{}) error {
+	return json.NewDecoder(r.Body).Decode(&body)
+}
+
+// Write encodes the response for a request with its status code
 func Write(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.WriteHeader(statusCode)
 	if err := json.NewEncoder(w).Encode(data); err != nil {
@@ -13,16 +19,14 @@ func Write(w http.ResponseWriter, statusCode int, data interface{}) {
 	}
 }
 
-func Read(r *http.Request, body interface{}) error {
-	return json.NewDecoder(r.Body).Decode(&body)
-}
-
+// Success returns a success message for a request
 func Success(w http.ResponseWriter, statusCode int, message string) {
 	Write(w, statusCode, struct {
 		Message string `json:"message"`
 	}{message})
 }
 
+// Error returns the message with reason for failure for a request
 func Error(w http.ResponseWriter, statusCode int, err error) {
 	Write(w, statusCode, struct {
 		Error string `json:"error"`
