4e45ecf2b78efe44fa3290e946a4223c15f60cdf
Lauren Ferreira <lauren.ferremch@gmail.com>
2020-01-29 22:50:02 -0300

    Setup Repository's Create integration tests

--- article/repository_integration_test.go
+++ article/repository_integration_test.go
@@ -1,1 +1,15 @@
 package article
+
+import "testing"
+
+func TestRepository_Create(t *testing.T) {
+	t.Parallel()
+
+	t.Run("when repository fails to create article", func(t *testing.T) {
+		t.Parallel()
+	})
+
+	t.Run("when repository creates the article", func(t *testing.T) {
+		t.Parallel()
+	})
+}
