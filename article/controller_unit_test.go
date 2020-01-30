1508e922aa23c61f4a7fa27a57bcb872e7086f98
Lauren Ferreira <lauren.ferremch@gmail.com>
2020-01-30 17:20:45 -0300

    Create repository tests

--- article/controller_unit_test.go
+++ article/controller_unit_test.go
@@ -9,7 +9,7 @@ import (
 	assert "github.com/stretchr/testify/require"
 )
 
-func TestArticle_NewController(t *testing.T) {
+func TestController_NewController(t *testing.T) {
 	t.Parallel()
 	t.Run("when Controller is created", func(t *testing.T) {
 		t.Parallel()
@@ -22,7 +22,7 @@ func TestArticle_NewController(t *testing.T) {
 	})
 }
 
-func TestArticle_Create(t *testing.T) {
+func TestController_Create(t *testing.T) {
 	t.Parallel()
 	t.Run("when article creation fails", func(t *testing.T) {
 		t.Parallel()
@@ -43,5 +43,19 @@ func TestArticle_Create(t *testing.T) {
 	})
 	t.Run("when article creation succeeds", func(t *testing.T) {
 		t.Parallel()
+		is := assert.New(t)
+
+		r := new(MockRepository)
+		defer r.AssertExpectations(t)
+
+		r.On("Create", mock.Anything).
+			Return(nil).
+			Once()
+
+		c := NewController(r)
+
+		article := &Article{}
+		is.Nil(faker.FakeData(article))
+		is.Nil(c.Create(article))
 	})
 }
--- article/repository_integration_test.go
+++ article/repository_integration_test.go
@@ -1,15 +1,51 @@
 package article
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/bxcodec/faker"
+	"github.com/larien/clean-architecture/helper/database"
+	assert "github.com/stretchr/testify/require"
+)
 
 func TestRepository_Create(t *testing.T) {
-	t.Parallel()
-
 	t.Run("when repository fails to create article", func(t *testing.T) {
-		t.Parallel()
-	})
+		is := assert.New(t)
 
+		db, err := database.New("localhost", "larien", "clean_architecture", "")
+		if err != nil {
+			t.Fatalf("failed to connect to database: %v", err)
+		}
+
+		repository := NewRepository(db)
+		is.NotNil(repository)
+
+		db.Close()
+		article := &Article{}
+		is.Nil(faker.FakeData(article))
+		is.NotNil(repository.Create(article))
+	})
 	t.Run("when repository creates the article", func(t *testing.T) {
-		t.Parallel()
+		// is := assert.New(t)
+
+		// db, err := database.New("localhost", "larien", "clean_architecture", "")
+		// if err != nil {
+		// 	t.Fatalf("failed to connect to database: %v", err)
+		// }
+
+		// repository := NewRepository(db)
+		// is.NotNil(repository)
+
+		// article := &Article{}
+		// is.Nil(faker.FakeData(article))
+		// article.ID = 0
+		// is.Nil(repository.Create(article))
+		// time.Sleep(time.Second)
+		// article2 := &Article{}
+		// res := db.First(article2, 7)
+		// fmt.Printf("1: %+v\n", article)
+		// fmt.Printf("2: %+v\n", article2)
+		// is.Nil(res.Error)
+		// is.Equal(article.ID, article2.ID)
 	})
 }
