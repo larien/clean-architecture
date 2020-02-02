0dac2521bab1f47d05de4a0a00af3f0b2629e5e8
Lauren Ferreira <lauren.ferremch@gmail.com>
2020-02-02 19:14:35 -0300

    Create List mocked method for Repository layer

--- article/controller_mock.go
+++ article/controller_mock.go
@@ -9,13 +9,13 @@ type MockController struct {
 	mock.Mock
 }
 
-// create represents the mocked method for Create feature in Controller layer
+// Create represents the mocked method for Create feature in Controller layer
 func (m *MockController) Create(article *Article) error {
 	args := m.Called(article)
 	return args.Error(0)
 }
 
-// list represents the mocked method for List feature in Controller layer
+// List represents the mocked method for List feature in Controller layer
 func (m *MockController) List() ([]*Article, error) {
 	args := m.Called(nil)
 	if args.Get(0) == nil {
